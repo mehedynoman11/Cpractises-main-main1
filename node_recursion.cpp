57bb588ea0afe87397c09dd45c32ab696f703137
mehedynoman11 <mehedynoman11@gmail.com>
2025-05-17 21:48:17 +0600

    Node print by recursion

---
+++ node_recursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Node {
+    public:
+        int val;
+        Node* next;
+        Node (int val) {
+            this->val=val;
+            this->next=NULL;
+        }
+};
+void print_recursion(Node* n) {
+    if (n == NULL) return; 
+    cout<<n->val<<" ";
+    print_recursion(n->next);
+}
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+
+    print_recursion(head);
+    return 0;
+}
